62bdebfca2da4fd194c047f5c31c8bf414f1a96e
TB2715 <hyunjivv@naver.com>
2020-02-03 22:39:03 +0900

    Updates

---
+++ precache-manifest.92623d7f64d9191a58a6f355af2ed9a1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51fb8f9ce439db7743b7e7fce2bf4eda",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b000c566606753568897",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "56fb30fe7eafda939999",
+    "url": "/portfolio/static/css/main.88931e8b.chunk.css"
+  },
+  {
+    "revision": "b000c566606753568897",
+    "url": "/portfolio/static/js/2.46034739.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/portfolio/static/js/2.46034739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56fb30fe7eafda939999",
+    "url": "/portfolio/static/js/main.d3110a08.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "8454de7f6b5913a2032f273bbb374089",
+    "url": "/portfolio/static/media/KSC2019.8454de7f.png"
+  },
+  {
+    "revision": "562cdf72401fe8a31a709b6d5900b2f4",
+    "url": "/portfolio/static/media/Knocap_web.562cdf72.png"
+  },
+  {
+    "revision": "c4723de3da750cff5d95b7c9b398d983",
+    "url": "/portfolio/static/media/NotoSansKR-Bold.c4723de3.otf"
+  },
+  {
+    "revision": "1b0504a3e741f8e6e07b297cb8f55d2d",
+    "url": "/portfolio/static/media/NotoSansKR-Light.1b0504a3.otf"
+  },
+  {
+    "revision": "3e344d255a9c2bc603683f5b078d471d",
+    "url": "/portfolio/static/media/NotoSansKR-Medium.3e344d25.otf"
+  },
+  {
+    "revision": "3c91862c95752f24cd41785301373e7b",
+    "url": "/portfolio/static/media/NotoSansKR-Thin.3c91862c.otf"
+  },
+  {
+    "revision": "ceae9c71021cfc0c5ed8a423b4d7ca3d",
+    "url": "/portfolio/static/media/Profile.ceae9c71.png"
+  },
+  {
+    "revision": "31d0922af71b95abbd8111023a4cb1c0",
+    "url": "/portfolio/static/media/WalWalTalk_facebookMsg.31d0922a.png"
+  }
+]);
